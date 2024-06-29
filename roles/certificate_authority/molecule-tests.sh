b7c5bc92e1c8828a6a0ecf93738dcc7cca637be3
Jim Tarpley <46848743+trippsc2@users.noreply.github.com>
2024-06-29 18:38:04 -0400

    Reworked molecule tests.

--- roles/certificate_authority/molecule-tests.sh
+++ roles/certificate_authority/molecule-tests.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-MOLECULE_BOX="w2019_cis" molecule test
 MOLECULE_BOX="w2022_cis" molecule test
+MOLECULE_BOX="w2019_cis" molecule test
--- roles/signed_certificate/molecule-tests.sh
+++ roles/signed_certificate/molecule-tests.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-MOLECULE_BOX="w2019_cis" molecule test -s win
-MOLECULE_BOX="w2022_cis" molecule test -s win
-
 MOLECULE_BOX="rocky8_cis" molecule test -s linux
+MOLECULE_BOX="w2022_cis" molecule test -s win
+MOLECULE_BOX="debian12_base" molecule test -s linux
+MOLECULE_BOX="ubuntu2404_base" molecule test -s linux
+
+MOLECULE_BOX="w2019_cis" molecule test -s win
+
 MOLECULE_BOX="rocky9_base" molecule test -s linux
 
-MOLECULE_BOX="debian12_base" molecule test -s linux
 MOLECULE_BOX="debian11_base" molecule test -s linux
 
-MOLECULE_BOX="ubuntu2404_base" molecule test -s linux
 MOLECULE_BOX="ubuntu2204_base" molecule test -s linux
 MOLECULE_BOX="ubuntu2004_base" molecule test -s linux
